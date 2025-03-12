aa21d5d801d85691dd5fb7e03778b409d68ee7ef
Bryna Hazelton <bryna.hazelton@gmail.com>
2025-03-12 09:05:21 -0700

    inital package setup

---
+++ src/heratape/base.py
@@ -0,0 +1,178 @@
+# Copyright (c) 2025 HERA-Team
+# Licensed under the MIT License
+"""Define the database base objects."""
+
+import json
+import os
+from abc import ABCMeta
+from datetime import date
+
+import numpy as np
+from sqlalchemy import create_engine
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import declarative_base
+from sqlalchemy.orm.session import sessionmaker
+
+config_file = os.path.expanduser("~/.heratape/heratape_config.json")
+
+
+def get_db_url(testing=False):
+    """Get the database file from the config file."""
+    with open(config_file) as f:
+        config_data = json.load(f)
+
+    if testing:
+        db_url = config_data.get("test_db_url")
+    else:
+        db_url = config_data.get("default_db_url")
+    if "postgresql" in db_url and "postgresql+psycopg" not in db_url:
+        db_url = db_url.replace("postgresql", "postgresql+psycopg")
+    if db_url is None:  # pragma: no cover
+        raise RuntimeError(
+            "cannot get heratape database file: no default_db_url "
+            f"listed in the config file: {config_file}"
+        )
+
+    return db_url
+
+
+class Base:
+    """Base table object."""
+
+    def __repr__(self):
+        """Define standard representation."""
+        columns = self.__table__.columns.keys()
+        rep_str = "<" + self.__class__.__name__ + "("
+        for c in columns:
+            rep_str += str(getattr(self, c)) + ", "
+        rep_str = rep_str[0:-2]
+        rep_str += ")>"
+        return rep_str
+
+    def isclose(self, other):
+        """Test if two objects are nearly equal."""
+        if not isinstance(other, self.__class__):
+            print("not the same class")
+            return False
+
+        self_columns = self.__table__.columns
+        other_columns = other.__table__.columns
+        # the following is structured as an assert because I cannot make it fail but
+        # think it should be checked.
+        assert {col.name for col in self_columns} == {
+            col.name for col in other_columns
+        }, (
+            "Set of columns are not the same. This should not happen, please make an "
+            "issue in our repo."
+        )
+        for col in self_columns:
+            self_col = getattr(self, col.name)
+            other_col = getattr(other, col.name)
+            if not isinstance(other_col, type(self_col)):
+                print(
+                    f"column {col} has different types, left is {type(self_col)}, "
+                    f"right is {type(other_col)}."
+                )
+                return False
+            if isinstance(self_col, bool):
+                # have to check bool before int because bool is a subclass of int
+                if self_col != other_col:
+                    print(f"column {col} is a boolean, values are not equal")
+                    return False
+            elif isinstance(self_col, int):
+                if self_col != other_col:
+                    print(f"column {col} is an int, values are not equal")
+                    return False
+            elif isinstance(self_col, str):
+                if self_col != other_col:
+                    print(f"column {col} is a str, values are not equal")
+                    return False
+            elif isinstance(self_col, date):
+                if self_col != other_col:
+                    print(f"column {col} is a datetime, values are not equal")
+                    return False
+            elif self_col is None:
+                # nullable columns, both null (otherwise caught as different types)
+                pass
+            else:
+                if hasattr(self, "tols") and col.name in self.tols:
+                    atol = self.tols[col.name]["atol"]
+                    rtol = self.tols[col.name]["rtol"]
+                else:
+                    # use numpy defaults
+                    atol = 1e-08
+                    rtol = 1e-05
+                if isinstance(self_col, np.ndarray | list):
+                    if not np.allclose(self_col, other_col, atol=atol, rtol=rtol):
+                        print(
+                            f"column {col} is a float-like array, values are not equal"
+                        )
+                        return False
+                else:
+                    if not np.isclose(self_col, other_col, atol=atol, rtol=rtol):
+                        print(f"column {col} is float-like, values are not equal")
+                        return False
+        return True
+
+
+Base = declarative_base(cls=Base)
+
+
+# revist whether this should be an ABC b/c of B024 linter error. Ignoring for now
+class DB(metaclass=ABCMeta):  # noqa B024
+    """
+    Abstract base class for heratape database object.
+
+    This ABC is only instantiated through the AutomappedDB or DeclarativeDB
+    subclasses.
+
+    """
+
+    engine = None
+    sessionmaker = sessionmaker()
+    sqlalchemy_base = None
+
+    def __init__(self, testing=False):  # noqa
+        db_url = get_db_url(testing=testing)
+        self.sqlalchemy_base = Base
+        self.engine = create_engine(db_url)
+        self.sessionmaker.configure(bind=self.engine)
+
+
+class DeclarativeDB(DB):
+    """Declarative database object -- to create database tables."""
+
+    def __init__(self, testing=False):
+        super().__init__(Base, testing=testing)
+
+    def create_tables(self):
+        """Create all tables."""
+        self.sqlalchemy_base.metadata.create_all(self.engine)
+
+    def drop_tables(self):
+        """Drop all tables."""
+        self.sqlalchemy_base.metadata.bind = self.engine
+        self.sqlalchemy_base.metadata.drop_all(self.engine)
+
+
+class AutomappedDB(DB):
+    """Automapped database object -- attaches to an existing database.
+
+    This is intended for use with the production database. __init__()
+    raises an exception if the existing database does not match the schema
+    defined in the SQLAlchemy initialization magic.
+
+    """
+
+    def __init__(self, testing=False):
+        super().__init__(automap_base(), testing=testing)
+
+        from .db_check import is_valid_database
+
+        with self.sessionmaker() as session:
+            db_valid, valid_msg = is_valid_database(Base, session)
+            if not db_valid:  # pragma: no cover
+                db_file = get_db_url(testing=testing)
+                raise RuntimeError(
+                    f"Database {db_file} does not match expected schema. " + valid_msg
+                )
---
+++ src/heratape/db_check.py
@@ -0,0 +1,83 @@
+# Copyright (c) 2025 HERA-Team
+# Licensed under the MIT License
+"""Database consistency checking functions."""
+
+from sqlalchemy import inspect
+from sqlalchemy.exc import OperationalError
+
+
+def is_valid_database(base, session):
+    """
+    Check that the current database matches the models declared in model base.
+
+    Currently we check that all tables exist with all columns.
+
+    What is not checked:
+
+    * Column types are not verified
+    * Relationships are not verified
+
+    Parameters
+    ----------
+    base : Declarative Base
+        Instance of SQLAlchemy Declarative Base to check.
+    session : SQLAlchemy session
+        Session to use, bound to an engine.
+
+    Returns
+    -------
+    True if all declared models have corresponding tables and columns.
+
+    """
+    if base is None:
+        from .base import Base
+
+        base = Base
+
+    engine = session.get_bind()
+    try:  # This tries thrice with 5sec sleeps in between
+        iengine = inspect(engine)
+    except OperationalError:  # pragma: no cover
+        import time
+
+        time.sleep(5)
+        try:
+            iengine = inspect(engine)
+        except OperationalError:
+            time.sleep(5)
+            iengine = inspect(engine)
+
+    errors = False
+    err_msg = ""
+
+    tables = iengine.get_table_names()
+
+    # Go through all SQLAlchemy models
+
+    for table, klass in base.metadata.tables.items():
+        if table in tables:
+            # Check all columns are found
+            # Looks like [{'default':
+            #               "nextval('validity_check_test_id_seq'::regclass)",
+            #              'autoincrement': True, 'nullable': False,
+            #              'type': INTEGER(), 'name': 'id'}]
+
+            columns = [c["name"] for c in iengine.get_columns(table)]
+            mapper = inspect(klass)
+
+            for column in mapper.columns:
+                # Assume normal flat column
+                if column.key not in columns:
+                    err_msg = (
+                        f"Model {klass} declares column {column.key} which "
+                        f"does not exist in database {engine}"
+                    )
+                    errors = True
+        else:
+            err_msg = (
+                f"Model {klass} declares table {table} which does not "
+                f"exist in database {engine}"
+            )
+            errors = True
+
+    return not errors, err_msg
