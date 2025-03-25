505976bac464218b6675424779a7aacac92d09d0
Bryna Hazelton <bryna.hazelton@gmail.com>
2025-03-24 18:32:35 -0700

    add coverage for tapes

--- tests/test_tapes.py
+++ tests/test_tapes.py
@@ -3,18 +3,22 @@
 """Test tapes."""
 
 import datetime
+import re
 
+import pytest
 from astropy.time import Time
 
 from heratape import Tapes
 from heratape.tapes import add_tape, get_tape
 
 
-def test_add_tape(test_session):
+@pytest.mark.parametrize(
+    "purchase_date", [Time("2025-01-15"), datetime.datetime(2025, 1, 15)]
+)
+def test_add_tape(test_session, purchase_date):
     tape_id = "HERA_01"
     tape_type = "foo"
     size = int(8e12)
-    purchase_date = Time("2025-01-15")
 
     add_tape(
         tape_id=tape_id,
@@ -40,3 +44,39 @@ def test_add_tape(test_session):
 
     tape_obj = get_tape("foo", testing=True)
     assert tape_obj is None
+
+
+@pytest.mark.parametrize(
+    ("param", "value", "err_msg"),
+    [
+        (
+            "purchase_date",
+            "2025-01-15",
+            "purchase date must be a datetime or astropy Time object",
+        ),
+        (
+            "size",
+            int(1e6),
+            re.escape(
+                "size is less than 1TB (note the units are bytes). size: 1000000"
+            ),
+        ),
+    ],
+)
+def test_add_tape_errors(test_session, param, value, err_msg):
+    tape_id = "HERA_01"
+    tape_type = "foo"
+    size = int(8e12)
+    purchase_date = datetime.datetime(2025, 1, 15)
+
+    kwargs = {
+        "tape_id": tape_id,
+        "tape_type": tape_type,
+        "size": size,
+        "purchase_date": purchase_date,
+        "session": test_session,
+    }
+    kwargs[param] = value
+
+    with pytest.raises(ValueError, match=err_msg):
+        add_tape(**kwargs)
