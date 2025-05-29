c6c0d0dd339a68019ad1e34a908f635122295e0e
Danny Jacobs <jacobsda@sas.upenn.edu>
2025-05-29 13:55:45 -0600

    replace popen with error trapping call, fix Empty bug

--- src/heratape/tape_system.py
+++ src/heratape/tape_system.py
@@ -1,20 +1,38 @@
 import subprocess
 from .tapes import add_new_tapes_to_db,list_tapes_in_db
+import logging
+logging.basicConfig(                                                                                                            
+    level=logging.DEBUG if os.environ.get('LOG_LEVEL') == 'DEBUG' else logging.INFO,                                            
+    format='%(asctime)s - %(levelname)s - %(message)s'                                                                          
+)      
+logger = logging.getLogger('heratape-system')
+
+def run(cmd):
+    "Safely run cmd. report errors to logger then EXIT"
+    with subprocess.Popen(cmd,
+        shell=True,
+        stdout=subprocess.PIPE, stderr = subprocess.PIPE) as proc:
+                stdout,stderr = proc.communicate()                                                                          
+                lines = stdout.decode().split()                                                                             
+        if proc.returncode >0:
+            logger.error(f'{cmd} exited with code {proc.returncode},             
+            error follow')                                                                                                  
+            logger.error(stderr)                                                                                            
+            sys.exit(proc.returncode)
+        return lines
 
 def tape_in_drive(drivenum):                                                                                                                                                                                                                            
     #input drivenum = 0 or 1                                                                                                                                                                                                                            
     #returns: VolumeTag of tape in drive                                                                                                                                                                                                                
     assert(drivenum in [0,1])                                                                                              
-    with subprocess.Popen('sudo mtx -f /dev/sg25 status', shell=True, stdout=subprocess.PIPE) as proc:                  
-            lines = proc.stdout.readlines()                                                                                
+    lines = run('sudo mtx -f /dev/sg25 status')
     for line in lines:                                                                                                      
         l = line.decode('utf8')                                                                                         
         if l.startswith(f'Data Transfer Element {drivenum}'):                                                               
             return l.split('=')[1].strip()    
 def query_tape_jukebox():                                                                                                   
     #return a list of tapes in the storage rac                                                                              
-    with subprocess.Popen('sudo mtx -f /dev/sg25 status', shell=True, stdout=subprocess.PIPE) as proc:                                                                                                                                                  
-            lines = proc.stdout.readlines()                                                                                                                                                                                                             
+    lines = run('sudo mtx -f /dev/sg25 status')
     tapes = []                                                                                                              
     for line in lines:                                                                                                                                                                                                                                  
         l = line.decode('utf8').strip()                                                                                     
@@ -27,31 +45,27 @@ def query_tape_jukebox():
 def find_empty_slots():                                                                                                     
     #return a list of empty slot numbers                                                                                    
     #return a list of tapes in the storage rac                                                                                                                                                                                                          
-    with subprocess.Popen('sudo mtx -f /dev/sg25 status', shell=True, stdout=subprocess.PIPE) as proc:                                                                                                                                                  
-            lines = proc.stdout.readlines()                                                                                                                                                                                                             
+    lines = run('sudo mtx -f /dev/sg25 status')
     empty_slots = []                                                                                                                                                                                                                                    
     for line in lines:                                                                                                      
         l = line.decode('utf8').strip()                                                                                     
-        if l.endswith('Empty'):                                                                                             
+        if l.endswith('Empty') and l.startswith('Storage'):          
             empty_slots.append(l.split(':')[0].split()[2])                                                                  
     return empty_slots                                                                                                      
 def unload_tape(drive, toslot):                                                                                             
     #input: drive number 0 or 1, and toslot destination to unload to                                                        
-    with subprocess.Popen(f'sudo mtx -f /dev/sg25 unload {toslot} {drive}', shell=True, stdout=subprocess.PIPE) as proc:                                                                                                                                
-            lines = proc.stdout.readlines()                                                                                 
+    lines = run(f'sudo mtx -f /dev/sg25 unload {toslot} {drive}')
     #TODO: trap for error where the slot isn't empty                                                                        
     # there are probably other error conditions I don't know about yet                                                      
 def load_tape(drive,fromslot):                                                                                              
     #input: drive number 0 or 1, and a fromslot to grab from                                                                
-    with subprocess.Popen(f'sudo mtx -f /dev/sg25 load {fromslot} {drive} ', shell=True, stdout=subprocess.PIPE) as proc:                                                                                                                               
-            lines = proc.stdout.readlines()                                                                                 
+    lines = run(f'sudo mtx -f /dev/sg25 load {fromslot} {drive}')
     # TODO: don't know what error conditions to trap for here. but they'll come       
 def list_tapes_in_system():
     # on herastore01 on NRAO
     # return list of all tapes in system
     #get the list of tapes currently in the jukebox
-    with subprocess.Popen('sudo mtx -f /dev/sg25 status', shell=True, stdout=subprocess.PIPE) as proc:
-        lines = proc.stdout.readlines()
+    lines = run('sudo mtx -f /dev/sg25 status')
     
     tapes = []
     for line in lines:
@@ -63,8 +77,7 @@ def tape_in_drive(drivenum):
     #input drivenum = 0 or 1
     #returns: VolumeTag of tape in drive
     assert(drivenum in [0,1])
-    with subprocess.Popen('sudo mtx -f /dev/sg25 status', shell=True, stdout=subprocess.PIPE) as proc:                                  lines = proc.stdout.readlines()    
-
+    lines - run('sudo mtx -f /dev/sg25 status')
     for line in lines:
         s = line.decode('utf8')
         if s.startswith(f'Data Transfer Element {drivenum}'):
@@ -85,8 +98,7 @@ def update_tapes(testing=False):
     print(f'adding {len(newtapes)} new tapes')
     add_new_tapes_to_db(newtapes)
 def check_drive_online(drivenum,testing=False):
-    with subprocess.Popen(f'sudo mt -f /dev/nst{drivenum} status', shell=True, stdout=subprocess.PIPE) as proc:
-        lines = proc.stdout.readlines()
+    lines = run(f'sudo mt -f /dev/nst{drivenum} status')
     for line in lines:
         if 'ONLINE' in  line.decode('utf8'): return True
     return False
